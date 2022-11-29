1b98d032127dba259f397724649dcff81e408177
Wesley-waka <wesley.waka@student.moringaschool.com>
2022-11-29 14:01:40 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit',(e) => {
+    e.preventDefault()
+    makeTask(e.target['new-task-description'].value)
+    form.requestFullscreen()
+  })
 });
+function makeTask (task){
+  let li = document.createElement('li');
+  let button = document.createDocument('button')
+  button.addEventListener('click', deleteTask)
+  button.textContent = "Delete"
+  li.textContent = task +" "
+  li.appendChild(button)
+  document.getElementById("tasks").appendChild(li)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
+
